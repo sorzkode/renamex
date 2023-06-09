b2455837271154a9ffc2d37f682524f22eb12953
RILEYJITSU <rileyjitsu@gmail.com>
2023-06-09 15:19:38 -0400

    updates

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
 import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name='renamex',
     version='1.0.0',
     description='Bulk file renaming utility.',
     url='https://github.com/sorzkode/',
-    author='sorzkode',
+    author='Mister Riley',
     author_email='<sorzkode@proton.me>',
     packages=setuptools.find_packages(),
     install_requires=['pyfiglet', 'click', 'tkinter'],
     long_description='Renamomicon Ex-Bulkus...the script of the dead. AKA an Evil Dead themed bulk file renaming utility.',
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: MIT',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        ],
+    ],
 )
