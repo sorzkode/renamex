d2d6fee72b8fbfe8b90af00db8bf14b94f161316
RILEYJITSU <rileyjitsu@gmail.com>
2023-12-24 11:44:38 -0500

    updates

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='renamex',
-    version='1.0.0',
+    version='2.0.0',
     description='Bulk file renaming utility.',
     url='https://github.com/sorzkode/',
     author='Mister Riley',
